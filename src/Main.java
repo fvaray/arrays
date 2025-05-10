434c66966ed865b6f2dceedd69967e27ec20852e
fvaray <fvaray@gmail.com>
2025-05-10 15:53:33 +0700

    Исправил замечания наставника

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,25 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
-        int [] arrayInt = new int [3];
-        for (int i = 0; i < 3; i++) {
+        int[] arrayInt = new int[3];
+        for (int i = 0; i < arrayInt.length; i++) {
             arrayInt[i] = i + 1;
         }
-        System.out.println();
-        float [ ] arrayFloat = {1.57f , 7.654f, 9.986f};
-        char [] arrayChar = {'A', 'B', 65, 66, '#'};
-        System.out.println();
+        float[] arrayFloat = {1.57f, 7.654f, 9.986f};
+        char[] arrayChar = {'A', 'B', 65, 66, '#'};
 
         System.out.println("Задача 2");
         System.out.printf("%d, %d, %d \n", arrayInt[0], arrayInt[1], arrayInt[2]);
         System.out.printf("%.2f, %.3f, %.3f \n", arrayFloat[0], arrayFloat[1], arrayFloat[2]);
         int i = 0;
-        for (; i < arrayChar.length-1; i++) {
+        for (; i < arrayChar.length - 1; i++) {
             System.out.printf(arrayChar[i] + ", ");
         }
         System.out.println(arrayChar[i]);
 
         System.out.println("Задача 3");
         System.out.printf("%d, %d, %d \n", arrayInt[2], arrayInt[1], arrayInt[0]);
-        System.out.printf("%.2f, %.3f, %.3f \n", arrayFloat[2], arrayFloat[1], arrayFloat[0]);
+        System.out.printf("%.3f, %.3f, %.2f \n", arrayFloat[2], arrayFloat[1], arrayFloat[0]);
         i = arrayChar.length - 1;
         for (; i > 0; i--) {
             System.out.printf(arrayChar[i] + ", ");
@@ -34,7 +32,7 @@ public class Main {
 
         System.out.println("Задача 4");
         for (i = 0; i < arrayInt.length; i++) {
-            if(arrayInt[i] % 2 != 0)
+            if (arrayInt[i] % 2 != 0)
                 arrayInt[i] += 1;
         }
         System.out.println(Arrays.toString(arrayInt));
