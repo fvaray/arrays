ef3bbc6dbaeeff55ab2c232da30cebb6f8c976af
fvaray <fvaray@gmail.com>
2025-05-10 13:13:24 +0700

    Задачи 1..4 массивы

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Задача 1");
+        int [] arrayInt = new int [3];
+        for (int i = 0; i < 3; i++) {
+            arrayInt[i] = i + 1;
         }
+        System.out.println();
+        float [ ] arrayFloat = {1.57f , 7.654f, 9.986f};
+        char [] arrayChar = {'A', 'B', 65, 66, '#'};
+        System.out.println();
+
+        System.out.println("Задача 2");
+        System.out.printf("%d, %d, %d \n", arrayInt[0], arrayInt[1], arrayInt[2]);
+        System.out.printf("%.2f, %.3f, %.3f \n", arrayFloat[0], arrayFloat[1], arrayFloat[2]);
+        int i = 0;
+        for (; i < arrayChar.length-1; i++) {
+            System.out.printf(arrayChar[i] + ", ");
+        }
+        System.out.println(arrayChar[i]);
+
+        System.out.println("Задача 3");
+        System.out.printf("%d, %d, %d \n", arrayInt[2], arrayInt[1], arrayInt[0]);
+        System.out.printf("%.2f, %.3f, %.3f \n", arrayFloat[2], arrayFloat[1], arrayFloat[0]);
+        i = arrayChar.length - 1;
+        for (; i > 0; i--) {
+            System.out.printf(arrayChar[i] + ", ");
+        }
+        System.out.println(arrayChar[i]);
+
+        System.out.println("Задача 4");
+        for (i = 0; i < arrayInt.length; i++) {
+            if(arrayInt[i] % 2 != 0)
+                arrayInt[i] += 1;
+        }
+        System.out.println(Arrays.toString(arrayInt));
     }
+
 }
